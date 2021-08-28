a1b0888f6cc4866c9197aa729fad99fc025d94f1
Lionel Lints <lionel.lints@gmail.com>
2021-08-27 17:40:59 -0700

    🪱 add some cases

--- src/Command/main.ts
+++ src/Command/main.ts
@@ -14,6 +14,7 @@ const main = async () => {
 
   console.log('then we create a command')
   const commandObj = new CommandQuery(specialLogQuery, { id: 's908f709s', name: 'johhnyObject' })
+  const commandObj2 = new CommandQuery(specialLogQuery, { id: 'o239u4lknm', name: 'johhnyNew' })
 
   console.log('then we set the command')
   invoker.setCommand(commandObj)
@@ -21,7 +22,10 @@ const main = async () => {
   const sleep = (m: any) => new Promise(r => setTimeout(r, m))
   console.log('1 sec')
   await sleep(1000)
-  console.log('2 sec')
+  console.log('other actions could take place while the command is encapsulated')
+  await sleep(1000)
+  console.log('3 sec')
+  invoker.setCommand(commandObj2)
   await sleep(1000)
 
   console.log('finally we use the invoker to run the command')

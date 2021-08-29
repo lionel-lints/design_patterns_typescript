f860dc19b8e72305d580822cf6a76643434fb119
Lionel Lints <lionel.lints@lever.co>
2021-08-28 17:45:41 -0700

    😇 state pattern begun

---
+++ src/State/README.md
@@ -0,0 +1,4 @@
+
+# State Pattern
+
+The state pattern is used in a similar way to the Strategy pattern, however the state pattern traverses between different algorithms based on the state of the object.
---
+++ src/State/State.ts
@@ -0,0 +1,64 @@
+interface DocumentState {
+  executeQueryPlan (): Error | void;
+  toggleLock(): DocumentState;
+  update(newText: string): LockedOriginalDocument | LockedEditedDocument | EditableEditedDocument;
+}
+
+
+class LockedOriginalDocument implements DocumentState {
+  constructor(readonly text: string, readonly isLocked: true){
+  }
+  executeQueryPlan (){
+    console.log('LockedOriginalDocument query plan --> no changes, so no query issued')
+  }
+  toggleLock(){
+    return new EditableOriginalDocument(this.text, !this.isLocked)
+  }
+  update(newText: string){
+    console.error(`It is not possible to update a LockedOriginalDocument with ${newText}, UNLOCK it FIRST`)
+    return new LockedOriginalDocument(this.text, this.isLocked) 
+  }
+}
+
+class EditableOriginalDocument implements DocumentState {
+  constructor(readonly text: string, readonly isLocked: false){
+  }
+  executeQueryPlan (){
+    console.log('EditableOriginalDocument query plan --> no changes, so no query issued')
+  }
+  toggleLock(){
+    return new LockedOriginalDocument(this.text, !this.isLocked)
+  }
+  update(newText: string){
+    return new EditableEditedDocument(newText, this.isLocked)
+  }
+}
+
+class LockedEditedDocument implements DocumentState {
+  constructor(readonly text: string, readonly isLocked: true){
+  }
+  executeQueryPlan (){
+    console.log('LockedEditedDocument query plan would be issued!')
+  }
+  toggleLock(){
+    return new EditableEditedDocument(this.text, !this.isLocked)
+  }
+  update(newText: string){
+    console.error(`It is not possible to update a LockedOriginalDocument with ${newText}`)
+    return new LockedOriginalDocument(this.text, this.isLocked) 
+  }
+}
+
+class EditableEditedDocument implements DocumentState {
+  constructor(readonly text: string, readonly isLocked: false){
+  }
+  executeQueryPlan (){
+    console.log('EditableEditedDocument query plan would be issued!')
+  }
+  toggleLock(){
+    return new LockedEditedDocument(this.text, !this.isLocked);
+  }
+  update(newText: string){
+    return new EditableEditedDocument(newText, this.isLocked)
+  }
+}
